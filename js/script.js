edd27db6d2dccffe317caaf5b5aec697228d9249
edmedev <edem0142@gmail.com>
2023-06-30 00:14:13 -0500

    Creando la sección de Proyectos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Función para hacer clic al menu movil y desplegar la navegación:
+
 var iconoMenu = document.getElementById("icono-menu");
 var menusNavegacion = document.getElementsByClassName("navegacion__bloqueLista");
 
@@ -10,4 +12,34 @@ iconoMenu.addEventListener("click", function() {
     }
   }
 });
+
+
+//Funciones de botones para ver repositorio de proyectos:
+
+function repositorioEncriptador() {
+  window.open("https://github.com/edmedev/encriptador", "_blank");
+}
+
+function repositorioEdmeMovies() {
+  window.open("https://github.com/edmedev/edme-movies", "_blank");
+}
+
+function repositorioFigurasGeometricas() {
+  window.open("https://github.com/edmedev/figuras-geometricas", "_blank");
+}
+
+
+//Funciones de botones para ver la demo de proyectos:
+
+function demoEncriptador() {
+  window.open("https://edmedev.github.io/encriptador/", "_blank");
+}
+
+function demoEdmeMovies() {
+  window.open("https://edmedev.github.io/edme-movies/", "_blank");
+}
+
+function demoFigurasGeometricas() {
+  window.open("https://edmedev.github.io/figuras-geometricas/", "_blank");
+}
 
